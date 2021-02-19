c0d3e59ddc8b77cc2492da259bfa3931db67d9eb
Frederic GAURAT <fgaurat@eolem.com>
2021-02-19 11:45:59 +0100

    add flatlist

--- components/TodoList/TodoList.js
+++ components/TodoList/TodoList.js
@@ -1,20 +1,41 @@
-import React,{useEffect,useState} from 'react'
-import { ScrollView, Text } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { StyleSheet,FlatList, ScrollView, Text, View, TouchableHighlight, Alert } from "react-native";
 
+const renderItem = ({ item:todo } ) => (
+   <TouchableHighlight onPress={() => Alert.alert("Todo Info",todo.title)}>
+    <Text style={styles.item}>{todo.title}</Text>
+    </TouchableHighlight>
+);
 
 export default function TodoList() {
+  const [todos, setTodos] = useState([]);
 
-    const [todos, setTodos] = useState([])
+  
+  useEffect(() => {
+    fetch("http://192.168.1.13:3000/todos")
+      .then((resp) => resp.json())
+      .then((todos) => setTodos(todos));
+  }, []);
+  return (
+    <FlatList
+      data={todos}
+      renderItem={renderItem}
+      keyExtractor={(todo) => todo.id}
+    />
 
-    useEffect(() => {
-        fetch('http://192.168.1.13:3000/todos')
-        .then(resp => resp.json())
-        .then(todos => setTodos(todos))
-    }, [])
-    return (
-        <ScrollView>
-            {todos.map( todo => <Text key={todo.id}>{todo.title}</Text>)}
-            
-        </ScrollView>
-    )
+  );
 }
+
+
+const styles = StyleSheet.create({
+    item:{
+        padding:10,
+        marginBottom:5,
+        fontSize:20,
+        backgroundColor:'lightpink',
+        marginHorizontal:10,
+        marginTop:24, 
+    }
+
+  });
+  
